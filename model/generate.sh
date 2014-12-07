d176ec5934f28dad5bd87675547d1fc916066f2f
Ryndzionek Mariusz <mryndzionek@gmail.com>
2014-12-07 13:47:03 +0100

    Added parallel element support
    Added new example
    Fixed some minor issues

--- model/generate.sh
+++ model/generate.sh
@@ -10,5 +10,6 @@ fi
 
 gsl -q -script:scxml2plantuml blink.scxml
 gsl -q -script:scxml2plantuml microwave-01.scxml
+gsl -q -script:scxml2plantuml microwave-02.scxml
 gsl -q -script:scxml2plantuml calc.scxml
 java -jar ~/plantuml.jar -o ../img/ ../gen/
